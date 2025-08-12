01981981ca82d5518885e45f488cf7023a9bbea2
yunha <choiyunha531@gmail.com>
2025-08-12 12:25:13 +0900

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,28 +16,38 @@ kubectl delete -f order-service.yaml --ignore-not-found=true
 kubectl delete -f payment-service.yaml --ignore-not-found=true
 kubectl delete secrets --all --ignore-not-found=true
 kubectl delete configmaps --all --ignore-not-found=true
-kubectl delete -f secret-provider-class.yaml --ignore-not-foud=true
+kubectl delete -f secret-provider-class.yaml --ignore-not-found=true
 kubectl delete pvc --all
+kubectl delete -f secrets-test.yaml
+
+# CSI Driver 권한 부여
+kubectl apply -f csi-driver-rbac.yaml
+
+# CSI Driver 재시작
+kubectl rollout restart daemonset/csi-secrets-store-secrets-store-csi-driver -n kube-system
 
 # 2. 올바른 SecretProviderClass 적용
-kubectl apply -f secret-provider-class-correct.yaml
+kubectl apply -f secret-provider-class.yaml
+kubectl get SecretProviderClass
 
 # 3. 시크릿 생성 테스트 Job
 kubectl apply -f secrets-test.yaml
+kubectl get pod -w
+# running -> completed 확인 후 다음 진행
 
 # 4. 시크릿 생성 확인
 kubectl get secrets | grep kubox
 
 # 5. 인프라 서비스 배포
-kubectl apply -f mysql-secrets.yaml
-kubectl apply -f redis-simple.yaml
+kubectl apply -f mysql.yaml
+kubectl apply -f redis.yaml
 
 # 6. 애플리케이션 서비스 배포
-kubectl apply -f user-service-secrets.yaml
-kubectl apply -f product-service-secrets.yaml
-kubectl apply -f cart-service-secrets.yaml
-kubectl apply -f order-service-secrets.yaml
-kubectl apply -f payment-service-secrets.yaml
+kubectl apply -f user-service.yaml
+kubectl apply -f product-service.yaml
+kubectl apply -f cart-service.yaml
+kubectl apply -f order-service.yaml
+kubectl apply -f payment-service.yaml
 
 # 7. 인그레스 배포
 kubectl apply -f ingress.yaml
